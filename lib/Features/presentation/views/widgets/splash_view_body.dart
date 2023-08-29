d932539e83ab33973becda024b5cbc6d784cb9ae
Alaa Eldin Emad <77211701+Alaa-Eldin-Emad@users.noreply.github.com>
2023-08-29 18:07:01 +0300

    Animations in Splash View

--- lib/Features/presentation/views/widgets/splash_view_body.dart
+++ lib/Features/presentation/views/widgets/splash_view_body.dart
@@ -2,10 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import '../../../../core/utils/assets.dart';
 
-class SplashViewBody extends StatelessWidget {
-  const SplashViewBody({
-    super.key,
-  });
+class SplashViewBody extends StatefulWidget {
+  const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody> with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> rightSlidingAnimation;
+  late Animation<Offset> leftSlidingAnimation;
+  @override
+  void initState() {
+    super.initState();
+    slidingAnimation();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,18 +26,43 @@ class SplashViewBody extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Image.asset(AssetsData.logo),
+          AnimatedBuilder(
+              animation: rightSlidingAnimation,
+              builder: (context, _) {
+                return SlideTransition(
+                    position: rightSlidingAnimation,
+                    child: Image.asset(AssetsData.logo));
+              }),
           const SizedBox(
             height: 5,
           ),
-          Text(
-            'Pages Unbound, Price Unfound',
-            textAlign: TextAlign.center,
-            style: GoogleFonts.dmSerifDisplay(
-                textStyle: const TextStyle(fontSize: 24)),
-          ),
+          AnimatedBuilder(
+              animation: leftSlidingAnimation,
+              builder: (context, _) {
+                return SlideTransition(
+                  position: leftSlidingAnimation,
+                  child: Text(
+                    'Pages Unbound, Price Unfound',
+                    textAlign: TextAlign.center,
+                    style: GoogleFonts.dmSerifDisplay(
+                        textStyle: const TextStyle(fontSize: 24)),
+                  ),
+                );
+              }),
         ],
       ),
     );
+  }
+
+  void slidingAnimation() {
+    animationController = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 1200));
+    rightSlidingAnimation =
+        Tween<Offset>(begin: const Offset(2, 0), end: Offset.zero)
+            .animate(animationController);
+    leftSlidingAnimation =
+        Tween<Offset>(begin: const Offset(-2, 0), end: Offset.zero)
+            .animate(animationController);
+    animationController.forward();
   }
 }
