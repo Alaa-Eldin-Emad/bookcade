8d3ff50f42bf90b123d325558e96f36dd8837c12
Alaa Eldin Emad <77211701+Alaa-Eldin-Emad@users.noreply.github.com>
2023-09-06 15:55:06 +0300

    handling Execptions

--- lib/Features/Home/data/repositories/home_repo_implementation.dart
+++ lib/Features/Home/data/repositories/home_repo_implementation.dart
@@ -2,31 +2,46 @@ import 'package:bookcade/Features/Home/data/model/book_model/book_model.dart';
 import 'package:bookcade/Features/Home/data/repositories/home_repository.dart';
 import 'package:bookcade/core/errors/failures.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+import '../../../../core/API/api.dart';
 
-import '../../../../core/utils/API/api.dart';
-
-class HomeRepoImplementation extends HomeRepo{
+class HomeRepoImplementation extends HomeRepo {
   final API api;
   HomeRepoImplementation(this.api);
   @override
-  Future<Either<Failures, List<BookModel>>> fetchBestSellerBooks() async{
+  Future<Either<Failures, List<BookModel>>> fetchNewestBooks() async {
     try {
-   var data = await api.getData(endPoint: 'volumes?Filtering=free-ebooks&Sorting=newest&q=subject:Programming');
-   List<BookModel>books =[];
-   for (var item in data['items']) {
-     books.add(BookModel.fromJson(item));
-   }
-   return right(books);
-    }
-    catch (e){
-      return left(ServerFailure());
+      var data = await api.getData(
+          endPoint:
+              'volumes?Filtering=free-ebooks&Sorting=newest&q=subject:Programming');
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.fromDioError(e));
+      }
+      return left(ServerFailure(e.toString()));
     }
   }
 
   @override
-  Future<Either<Failures, List<BookModel>>> fetchBooks() {
-    // TODO: implement fetchBooks
-    throw UnimplementedError();
+  Future<Either<Failures, List<BookModel>>> fetchBooks() async {
+    try {
+      var data = await api.getData(
+          endPoint: 'volumes?Filtering=free-ebooks&q=subject:Programming');
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.fromDioError(e));
+      }
+      return left(ServerFailure(e.toString()));
+    }
   }
-
 }
